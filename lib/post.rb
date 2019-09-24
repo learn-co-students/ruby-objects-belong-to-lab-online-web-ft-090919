14fce6e798045d2112ada6ddf76c9199ec525bba
Randel Jiao <randeljiao@gmail.com>
2019-09-24 19:13:11 +0000

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -1,16 +1,3 @@
 class Post
-
   attr_accessor :title, :author
-  def initialize(title)
-    @title = title
-  end
-
-  def author_name
-    if @author == nil
-      return nil
-    else
-      @author.name
-    end
-  end
 end
-© 2019 GitHub, Inc.
